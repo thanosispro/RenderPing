53350682a04285be810963e9553104f130a92f07
thanosispro <126332750+thanosispro@users.noreply.github.com>
2026-03-27 14:59:19 +0545

    Update ping_script.py

--- ping_script.py
+++ ping_script.py
@@ -1,6 +1,4 @@
-// Source - https://stackoverflow.com/a/79282587
-// Posted by Mama africa, modified by community. See post 'Timeline' for change history
-// Retrieved 2026-03-27, License - CC BY-SA 4.0
+
 
 import requests
 import schedule
